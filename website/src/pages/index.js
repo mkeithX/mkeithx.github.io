b4e55328779b93317aef696db6b8fef53ad02a7f
mkeithX <dev.keith@outlook.com>
2024-02-18 23:08:57 +0800

    deploy actions

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Learn more <span className="pl-1 animate-pulse">▎</span>
           </Link>
         </div>
       </div>
