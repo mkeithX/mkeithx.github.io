d73e24cd20b4e51cb77bf7486ab16fab3681e156
mkeithX <dev.keith@outlook.com>
2024-02-18 18:09:45 +0800

    after legacy

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -11,6 +11,7 @@ const config = {
   title: 'mkeithX',
   tagline: "It's me, mkeithX",
   favicon: 'img/favicon.ico',
+  titleDelimiter: '·',
   url: 'https://mkeithx.github.io',
   baseUrl: '/',
   organizationName: 'mkeithX', // Usually your GitHub org/user name.
@@ -54,19 +55,22 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'mkeithX',
+        // title: 'mkeithX',
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: 'mkjs Logo',
+          src: 'img/mkjs.svg',
+          target: '_self',
+          width: 100,
+          height: 100,
         },
         items: [
           {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
-            position: 'left',
+            position: 'right',
             label: 'Tutorial',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          {to: '/blog', label: 'Blog', position: 'right'},
           {
             href: 'https://github.com/mkeithX/',
             'aria-label': 'GitHub repository',
@@ -77,47 +81,12 @@ const config = {
       },
       footer: {
         style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
+        logo: {
+          alt: 'mkeithX',
+          src: 'img/mkjs.svg',
+          href: '/',
+          height: '50'
+        },
         copyright: `Copyright © ${new Date().getFullYear()} mkeithX.`,
       },
       prism: {
