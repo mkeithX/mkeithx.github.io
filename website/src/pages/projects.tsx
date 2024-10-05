dfe2193821e9e105ae80a56fd8aa221bc1b165f6
Keith Tan <me@kitimi.onmicrosoft.com>
2024-10-05 19:43:34 +0800

    add media

--- website/src/pages/projects.tsx
+++ website/src/pages/projects.tsx
@@ -1,65 +1,74 @@
 import React from "react";
 import Layout from "@theme/Layout";
-
 import { Project, ProjectData } from "../components/ProjectCards/Projects";
+import clsx from "clsx";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Heading from "@theme/Heading";
+import styles from "./index.module.css";
 
 const dataDir = "./data/projects";
+
+// Define the project data in a more structured way
 const projects: ProjectData[] = [
   {
     title: "Python Starters",
-    description:
-      "Basic Python examples for starters.",
+    description: "Basic Python examples for starters.",
     url: "https://github.com/mkeithX/python-starters",
-    image: require(`${dataDir}/default.png`),
+    image: require(`${dataDir}/default.png`).default,
   },
   {
     title: "MKX Website",
     description: "A unified docs for IT Developers and Astronomers.",
     url: "https://mkeithx.pages.dev",
     image: require(`${dataDir}/default.png`),
-
   },
   {
     title: "Kitimi Platforms",
-    description: "Hub for contributors, authors and mainteners within the platform.",
+    description: "Hub for contributors, authors and maintainers within the platform.",
     url: "https://kitiplex.github.io",
     image: require(`${dataDir}/ms-banner-kitiplex.png`),
-
   },
   {
     title: "IUIU",
     description: "Knowledge Base for awesome people by awesome people.",
     url: "https://kitiplex.github.io/iuiu",
     image: require(`${dataDir}/sc-iuiu.png`),
-
   },
   {
     title: "Flask Tutorial - Demo",
-    description: "Building web application with Python.",
+    description: "Building web applications with Python.",
     url: "https://github.com/mkeithX/flask-tutorial-demo",
     image: require(`${dataDir}/default.png`),
-
   },
   {
-    title: "Django Custom User model",
+    title: "Django Custom User Model",
     description: "Customize Django's base user model with PostgreSQL.",
     url: "/projects",
     image: require(`${dataDir}/default-soon.png`),
-
   },
 ];
 
 const title = "Projects";
-const description = "This section highlights some of the projects and demos  I've authored and contributed to.";
+const description = "This section highlights some of the projects and demos I've authored and contributed to.";
 
-export default function Projects(): JSX.Element {
+const Projects: React.FC = (): JSX.Element => {
   return (
     <Layout title={title} description={description}>
-      <main className="container container--fluid margin-vert--lg">
-        <h1>{"Welcome"}</h1>
-        <p>{description}</p>
-
-        <div className="row">
+      <main className={clsx("container margin-vert--lg")}>
+        <Heading as="h1" className="hero__title">
+          <span className={styles.heroTitleTextHtml}>
+            <div className={styles.heroBannerTitle}>
+            {"Welcome"}
+            </div>
+          </span>
+        </Heading>
+        <div className={styles.heroSubtitle}>
+          <span className={styles.heroCustomSubTitleHtml}>
+            <b>{description}</b>
+          </span>
+        </div>
+        <hr></hr>
+        <div className={clsx(styles.section,"row")}>
           {projects.map((project) => (
             <Project key={project.title} {...project} />
           ))}
@@ -67,4 +76,6 @@ export default function Projects(): JSX.Element {
       </main>
     </Layout>
   );
-}
+};
+
+export default Projects;
