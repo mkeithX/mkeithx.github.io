2f52cde06857a8451757212dfc4c0707088334b5
Keith Tan <me@kitimi.onmicrosoft.com>
2024-12-28 13:57:38 +0800

    chore: custom algolia search

---
+++ website/src/theme/SearchBar/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import SearchBar from '@theme-original/SearchBar';
+import type SearchBarType from '@theme/SearchBar';
+import type {WrapperProps} from '@docusaurus/types';
+import { DocSearch } from '@docsearch/react';
+
+import '@docsearch/css';
+// import '@styles/DocSearch.css';
+
+type Props = WrapperProps<typeof SearchBarType>;
+
+export default function SearchBarWrapper(props: Props): JSX.Element {
+  return (
+    <div>
+      <DocSearch
+        apiKey="b63e590c0b5a9ab7c0930991ea785aeb"
+        indexName="mkeithx"
+        appId="XVSOLYZXNV"
+        placeholder="Search like a boss..."
+
+        getMissingResultsUrl={({ query }) => {
+          
+          return `https://github.com/mkeithX/mkeithx.github.io/issues/new?title=${query}`;
+        
+        }}
+        
+        resultsFooterComponent={({ state }) => {
+          return <h3>{(state.context.nbHits as number)} Match found</h3>;
+        }}
+
+        maxResultsPerGroup={7}
+        insights={true}
+      />
+    </div>
+  );
+}
+
+export {default as SearchBar} from '@theme-original/SearchBar';
