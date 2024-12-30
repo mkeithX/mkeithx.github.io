420605d32750086c92351c0e3cd89d1979270983
Keith Tan <me@kitimi.onmicrosoft.com>
2024-12-30 18:40:02 +0800

    misc: announcement bar enabled

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -215,12 +215,12 @@ const config: Config = {
       disableSwitch: false,
       respectPrefersColorScheme: true,
     },
-    // announcementBar: {
-    //   id: `announcementBar_`,
-    //   content:
-    //     `Discover what's new in our <a target="_blank" rel="noopener noreferrer" href="/blog">latest blog updates!</a>`,
-    //   isCloseable: true,
-    // },
+    announcementBar: {
+      id: `announcementBar_`,
+      content:
+        `Perihelion: Parker Solar Probe <a target="_blank" rel="noopener noreferrer" href="https://eyes.nasa.gov/apps/solar-system/#/story/psp_perihelion?slide=slide_2">closest approach to the Sun ☀️</a>`,
+      isCloseable: true,
+    },
     docs: {
       versionPersistence: 'localStorage',
       sidebar: {
