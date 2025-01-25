71ef60721d81162ee1666c001b5288fc467ed601
Keith Tan <me@kitimi.onmicrosoft.com>
2025-01-26 00:29:37 +0800

    fix teamprofiles

--- website/src/components/TeamProfileCards/index.tsx
+++ website/src/components/TeamProfileCards/index.tsx
@@ -1,27 +1,15 @@
 import React, { type ReactNode } from "react";
 import Translate from "@docusaurus/Translate";
 import Link from "@docusaurus/Link";
-import Heading from "@theme/Heading";
-import styles from "../QuickStartCards/styles.module.css";
 import clsx from "clsx";
-
-import {
-  FontAwesomeIcon,
-  FontAwesomeIconProps,
-} from "@fortawesome/react-fontawesome";
-
-import {
-  faGithub,
-  faLinkedin,
-  faXTwitter,
-} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin, faXTwitter } from "@fortawesome/free-brands-svg-icons";
+import styles from "../QuickStartCards/styles.module.css";
 
 function WebsiteLink({ to, children }: { to: string; children?: ReactNode }) {
   return (
     <Link to={to}>
-      {children ?? (
-        <Translate id="team.profile.websiteLinkLabel">website</Translate>
-      )}
+      {children ?? <Translate id="team.profile.websiteLinkLabel">website</Translate>}
     </Link>
   );
 }
@@ -64,12 +52,12 @@ function TeamProfileCard({
           <div className="button-group button-group--block">
             {githubUrl && (
               <Link className="button button--sm button--secondary" href={githubUrl}>
-                <FontAwesomeIcon icon={faGithub} size="2x"></FontAwesomeIcon>
+                <FontAwesomeIcon icon={faGithub} size="2x" />
               </Link>
             )}
             {twitterUrl && (
               <Link className="button button--sm button--secondary" href={twitterUrl}>
-                <FontAwesomeIcon icon={faXTwitter} size="2x"></FontAwesomeIcon>
+                <FontAwesomeIcon icon={faXTwitter} size="2x" />
               </Link>
             )}
           </div>
@@ -80,85 +68,102 @@ function TeamProfileCard({
 }
 
 function TeamProfileCardCol(props: ProfileProps) {
-  return (
-    <TeamProfileCard {...props} className="col col--6 margin-bottom--lg" />
-  );
+  return <TeamProfileCard {...props} className="col col--6 margin-bottom--lg" />;
 }
+
+const teamMembers = [
+  {
+    name: "Keith Tan",
+    subTitle: "Maintainer @ Kitiplex",
+    githubUrl: "https://github.com/mkeithX",
+    twitterUrl: "https://twitter.com/mkeithtan",
+  },
+  {
+    name: "Venice",
+    subTitle: "Contributor @ Kitiplex",
+    githubUrl: "https://github.com/Portiaporkshap",
+    twitterUrl: "https://twitter.com/",
+  },
+  {
+    name: "Gina",
+    subTitle: "Contributor @ Kitiplex",
+    githubUrl: "https://github.com/botgina",
+    twitterUrl: "https://twitter.com/",
+  },
+  {
+    name: "Kiti",
+    subTitle: "Contributor @ Kitiplex",
+    githubUrl: "https://github.com/kitimi88",
+    twitterUrl: "https://twitter.com/",
+  },
+
+];
+
+const contributors = [
+  {
+    name: "ayitsmeprincess",
+    subTitle: "Contributor @ Kitiplex",
+    githubUrl: "https://github.com/ayitsmeprincess",
+    twitterUrl: "https://twitter.com/",
+  },
+
+  {
+    name: "Kean Ortiga",
+    subTitle: "Contributor @ Kitiplex",
+    githubUrl: "https://github.com/rkortiga",
+    twitterUrl: "https://twitter.com/",
+  },
+  {
+    name: "PrimmieDoll",
+    subTitle: "Contributor @ Kitiplex",
+    githubUrl: "https://github.com/PrimmieDoll",
+    twitterUrl: "https://twitter.com/",
+  },
+  {
+    name: "Joei",
+    subTitle: "Contributor @ Kitiplex",
+    githubUrl: "https://github.com/JoeiX",
+    twitterUrl: "https://twitter.com/angajoei",
+  },
+  {
+    name: "PriMx",
+    subTitle: "Contributor @ Kitiplex",
+    githubUrl: "https://github.com/PriMaxima",
+    twitterUrl: "https://twitter.com/",
+  },
+
+  {
+    name: "Topsy",
+    subTitle: "Contributor @ Kitiplex",
+    githubUrl: "https://github.com/topsykretts8520",
+    twitterUrl: "https://twitter.com/",
+  },
+  {
+    name: "MeganB",
+    subTitle: "Contributor @ Kitiplex",
+    githubUrl: "https://github.com/MeganB143",
+    twitterUrl: "#",
+  },
+  {
+    name: "Johanna Lorenz",
+    subTitle: "Contributor @ Kitiplex",
+    githubUrl: "https://github.com/JLorenz143",
+    twitterUrl: "https://twitter.com/",
+  },
+  {
+    name: "Miriam G",
+    subTitle: "Contributor @ Kitiplex",
+    githubUrl: "https://github.com/MiriamG416",
+    twitterUrl: "https://twitter.com/",
+  },
+];
 
 export function CoreTeamRow(): JSX.Element {
   return (
     <div className="row">
-      <TeamProfileCardCol
-        name="Keith Tan"
-        subTitle="Maintainer @ Kitiplex"
-        githubUrl="https://github.com/mkeithX"
-        twitterUrl="https://twitter.com/mkeithtan" 
-        children={"Guides and Code Samples from Kitimi. Although written in python, the same principles can be applied to other programming language."}>
-      </TeamProfileCardCol>
-      <TeamProfileCardCol
-        name="ayitsmeprincess"
-        subTitle="Contributor @ Kitiplex"
-        githubUrl="https://github.com/ayitsmeprincess"
-        twitterUrl="https://twitter.com/"         children={"Guides and Code Samples from Kitimi. Although written in python, the same principles can be applied to other programming language."}>
-      </TeamProfileCardCol>
-      <TeamProfileCardCol
-        name="Gina"
-        subTitle="Contributor @ Kitiplex"
-        githubUrl="https://github.com/botgina"
-        twitterUrl="https://twitter.com/" children={""}>
-        </TeamProfileCardCol>
-      <TeamProfileCardCol
-        name="Kean Ortiga"
-        subTitle="Contributor @ Kitiplex"
-        githubUrl="https://github.com/rkortiga"
-        twitterUrl="https://twitter.com/" children={""}      >
-        </TeamProfileCardCol>
-
-      {/* <TeamProfileCardCol
-        name="doorinel"
-        subTitle="Contributor @ Kitiplex"
-        githubUrl="https://github.com/doorinel"
-        twitterUrl="https://twitter.com/"
-      ></TeamProfileCardCol> */}
-      <TeamProfileCardCol
-        name="PrimmieDoll"
-        subTitle="Contributor @ Kitiplex"
-        githubUrl="https://github.com/PrimmieDoll"
-        twitterUrl="https://twitter.com/" children={""}      >
-        {/* {"Building the web before bedtime."} */}
-      </TeamProfileCardCol>
-
-      <TeamProfileCardCol
-        name="Venice"
-        subTitle="Contributor @ Kitiplex"
-        githubUrl="https://github.com/Portiaporkshap"
-        twitterUrl="https://twitter.com/" children={""}      >
-        {/* {"Trust me I'm an engineer."} */}
-      </TeamProfileCardCol>
-
-      <TeamProfileCardCol
-        name="Joei"
-        subTitle="Contributor @ Kitiplex"
-        githubUrl="https://github.com/JoeiX"
-        twitterUrl="https://twitter.com/angajoei" children={""}      ></TeamProfileCardCol>
-
-      <TeamProfileCardCol
-        name="PriMx"
-        subTitle="Contributor @ Kitiplex"
-        githubUrl="https://github.com/PriMaxima"
-        twitterUrl="https://twitter.com/" children={""}      >
-        {/* {"Teammate is about to update profile. Be right back!"} */}
-      </TeamProfileCardCol>
-
-
-
-      <TeamProfileCardCol
-        name="Kiti"
-        subTitle="Contributor @ Kitiplex"
-        githubUrl="https://github.com/kitimi88"
-        twitterUrl="https://twitter.com/" children={""}      ></TeamProfileCardCol>
-
-
+      {teamMembers.map((member, index) => (
+        <TeamProfileCardCol key={index} {...member} children="Guides and code samples from Kitimi. Although written in Python, the same principles can be applied to other programming languages." />
+      ))}
     </div>
   );
 }
@@ -166,29 +171,9 @@ export function CoreTeamRow(): JSX.Element {
 export function ContributorsTeamRow(): JSX.Element {
   return (
     <div className="row">
-      <TeamProfileCardCol
-        name="Topsy"
-        subTitle="Contributor @ Kitiplex"
-        githubUrl="https://github.com/topsykretts8520"
-        twitterUrl="https://twitter.com/" children={""}      ></TeamProfileCardCol>
-
-      <TeamProfileCardCol
-        name="MeganB"
-        subTitle="Contributor @ Kitiplex"
-        githubUrl="https://github.com/MeganB143"
-        twitterUrl="#" children={""}      ></TeamProfileCardCol>
-
-      <TeamProfileCardCol
-        name="Johanna Lorenz"
-        subTitle="Contributor @ Kitiplex"
-        githubUrl="https://github.com/JLorenz143"
-        twitterUrl="https://twitter.com/" children={""}      ></TeamProfileCardCol>
-
-      <TeamProfileCardCol
-        name="Miriam G"
-        subTitle="Contributor @ Kitiplex"
-        githubUrl="https://github.com/MiriamG416"
-        twitterUrl="https://twitter.com/" children={""}      ></TeamProfileCardCol>
+      {contributors.map((member, index) => (
+        <TeamProfileCardCol key={index} {...member} children="Guides and code samples from Kitimi. Although written in Python, the same principles can be applied to other programming languages." />
+      ))}
     </div>
   );
 }
