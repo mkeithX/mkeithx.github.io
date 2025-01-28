04a6e607785d84b41523b19d53cffa6af6be9da8
Keith Tan <me@kitimi.onmicrosoft.com>
2025-01-28 09:22:08 +0800

    chore: css cleanup in /website

--- website/src/components/HomepageFeatures/index.tsx
+++ website/src/components/HomepageFeatures/index.tsx
@@ -18,19 +18,17 @@ const FeatureList: FeatureItem[] = [
     Svg: require("@site/static/img/terminal_icon.svg").default,
     description: (
       <>
-Quick solutions with practical code snippets, shared gists and more.
+        Quick solutions with practical code snippets, shared gists, and helpful resources.
       </>
     ),
   },
-
-
   {
     scale: 0.8,
     title: "Container",
     Svg: require("@site/static/img/container_solid.svg").default,
     description: (
       <>
-        Provides a suite of services to improve website performance and security.
+        Comprehensive services to enhance website performance, security, and reliability.
       </>
     ),
   },
@@ -40,22 +38,20 @@ Quick solutions with practical code snippets, shared gists and more.
     Svg: require("@site/static/img/api_icon.svg").default,
     description: (
       <>
-        Build fast and relevant search experiences for their applications and websites.
+        Build fast and relevant search experiences for applications and websites efficiently.
       </>
     ),
   },
-  
   {
     scale: 0.8,
     title: "Code Made Easy",
     Svg: require("@site/static/img/rocket.svg").default,
     description: (
       <>
-        Designed with developers in mind for a seamless experience.
+        Designed with developers in mind for a seamless and intuitive experience.
       </>
     ),
   },
-
   {
     scale: 1.0,
     title: "Built with React",
@@ -71,19 +67,19 @@ Quick solutions with practical code snippets, shared gists and more.
 function Feature({ scale, title, Svg, description }: FeatureItem) {
   return (
     <>
-    <div className={clsx("col col--4 margin-bottom--lg")}>
-      <div className={"text--center"}>
-        <Svg
-          transform={"scale(" + scale + ")"}
-          className={styles.featureSvg}
-          role="img"
-        />
+      <div className={clsx("col col--4 margin-bottom--lg")}>
+        <div className={"text--center"}>
+          <Svg
+            transform={"scale(" + scale + ")"}
+            className={styles.featureSvg}
+            role="img"
+          />
+        </div>
+        <div className="text--center padding-horiz--sm">
+          <Heading as="h3">{title}</Heading>
+          <p>{description}</p>
+        </div>
       </div>
-      <div className="text--center padding-horiz--sm">
-        <Heading as="h3">{title}</Heading>
-        <p>{description}</p>
-      </div>
-    </div>
     </>
   );
 }
