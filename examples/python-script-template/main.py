f6879034d750fae4d5a85a090431d16e6e8dd02e
Keith Tan <me@kitimi.onmicrosoft.com>
2025-03-08 10:44:43 +0800

    fix(api): pytest in python-script-template

--- examples/python-script-template/main.py
+++ examples/python-script-template/main.py
@@ -2,16 +2,19 @@ import os
 import sys
 from utils.helper import greet, date
 
-def start_coding():
+
+def long_msg():
     print("You can add more Python code to this script to Extend functionality!")
 
 
 def main():
     os.system('cls' if os.name == 'nt' else 'clear')
-    name = input("Enter your name: ")
+    app_title = 'Python Script Example'
+    print(f'{"-" * 40}\n{" " * 8}{app_title}{" " * 8}\n{"-" * 40}')
+    name = str(input("Your name: "))
     print(greet(name))
     print(f"Today is {date()}.")
-    start_coding()
+    long_msg()
 
 
 if __name__ == "__main__":
