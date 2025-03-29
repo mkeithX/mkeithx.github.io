4b73ded59562ea1530d1fc5fa2a8c47a22ffb592
Keith Tan <me@kitimi.onmicrosoft.com>
2025-03-29 23:10:42 +0800

    fix: minor typos and brokenLinks

--- website/src/pages/tests/_noIndex.tsx
+++ website/src/pages/tests/_noIndex.tsx
@@ -10,7 +10,7 @@ export default function TestsComponent() {
             style={{
                 display: 'flex',
                 justifyContent: 'center',
-                alignItems: 'center',
+                // alignItems: 'center',
                 height: '50vh',
                 fontSize: '20px',
             }}>
