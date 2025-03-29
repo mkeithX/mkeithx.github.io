c601658f73fedd16b2b02802c2c2c2b290830f9f
Keith Tan <me@kitimi.onmicrosoft.com>
2025-03-29 23:16:37 +0800

    update redirects

--- website/redirects.ts
+++ website/redirects.ts
@@ -12,6 +12,7 @@ const redirects: ClientRedirects.Options['redirects'] = [
   { from: '/docs/devops/troubleshoot/powershell', to: '/docs/gists/powershell' },
   { from: '/docs/devops/troubleshoot/windows-hardware-issues', to: '/docs/how-tos/fix-windows-hardware-issues' },
   { from: '/docs/devops/guides-and-workarounds', to: '/docs/how-tos' },
-  { from: ['/docs/tutorials','/docs/examples'], to: '/docs/learn' }
+  { from: ['/docs/tutorials', '/docs/examples'], to: '/docs/learn' },
+  { from: ['/test', '/dogfooding', 'dogfood'], to: '/tests' }
 ];
 export default redirects;
